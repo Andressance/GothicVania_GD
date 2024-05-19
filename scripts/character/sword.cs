b665fb92609e0004daadd95482bc0bf3ab3a3012
Andressance <andressancenon@gmail.com>
2024-05-19 21:02:58 +0200

    Damage

--- scripts/character/sword.cs
+++ scripts/character/sword.cs
@@ -1,18 +1,26 @@
 using Godot;
 using System;
 
-// Create a class_name 
-
-public partial class Landing : State
+public partial class sword : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.canMove = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _on_body_entered(Node body)
+	{
+		foreach (Node ChildEnteredTree in body.GetChildren())
+		{
+			if (ChildEnteredTree is Damageable)
+			{
+				(ChildEnteredTree as Damageable).TakeDamage(10);
+			}
+		}
 	}
 }
--- scripts/enemies/Damageable.cs
+++ scripts/enemies/Damageable.cs
@@ -1,18 +1,27 @@
 using Godot;
 using System;
 
-// Create a class_name 
-
-public partial class Landing : State
+public partial class Damageable : Node
 {
+	[Export]
+	public int health = 20;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.canMove = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void TakeDamage(int damage)
+	{
+		health -= damage;
+		if (health <= 0)
+		{
+			GetParent().QueueFree();
+		}
+
 	}
 }
